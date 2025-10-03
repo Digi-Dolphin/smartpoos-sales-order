63c0b64ead099f41e80a82e91bf1d969ddf0803d
Taher <ttayabali52@gmail.com>
2025-10-03 15:08:32 +0300

    url change

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const token = localStorage.getItem('access');
 const axiosInstance = axios.create({
   // baseURL: `https://1c52587ba365.ngrok-free.app`,
 //  baseURL: `http://127.0.0.1:8000/`,
-baseURL:`https://8cb5262abe4d.ngrok-free.app/`
+baseURL:`https://25b01655f816.ngrok-free.app/`
 });
 
 axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
